ccae2ec1c8b4085a55c6a5a50c4c91ebb2b099dd
asdadads <jas31goh@gmail.com>
2022-06-27 16:31:30 +0800

    removed redundant x y parameter from checkWithinCa

--- sketch.js
+++ sketch.js
@@ -8,13 +8,17 @@ var stateHistory = [];
 var stateFuture = [];
 var stampImagesAr = Array(10);
 var sparkles;
+var selectMode;
+var selectedArea;
+var selectButton;
+var selectedPixels;
 
 function preload() {
 	sparkles = loadImage('./assets/sparkles.png');
 }
 
 function setup() {
-	document.addEventListener('contextmenu',e=>e.preventDefault()); //disable 
+	document.addEventListener('contextmenu',e=>e.preventDefault()); //disable right-click
 
 	//create a canvas to fill the content div from index.html
 	canvasContainer = select('#content');
@@ -42,12 +46,32 @@ function setup() {
 
 	background(255); //background must be present or undo/redo will not work
 	blankCanvas = get(); //for resetting canvas to clean white image when cleared
-}
 
-function setup() {
 	selectMode = 0;
-	selectButton = createButton('Select area');
-	selectButton.mousePressed
+	selectedArea = {x: -1, y: -1, w: 100, y: 100};
+	selectButton = select('#cutButton');
+	selectButton.mousePressed(function() {
+		if (selectMode == 0) {
+			selectMode += 1;
+			selectButton.html('Cut');
+			loadPixels();
+		} else if (selectMode == 1) {
+			selectMode += 1;
+			selectButton.html('End paste');
+			updatePixels();
+
+            selectedPixels = get(selectedArea.x,selectedArea.y,selectedArea.w,selectedArea.h);
+			noStroke();
+			//draw a rectangle over it
+			fill(255);
+			rect(selectedArea.x, selectedArea.y, selectedArea.w, selectedArea.h);
+		} else if (selectMode == 2) {
+			selectMode = 0;
+			loadPixels();
+			selectedArea = {x: -1, y: -1, w: 100, h: 100};
+			selectButton.html('Select area to cut');
+		}
+	})
 }
 
 function draw() {
@@ -58,14 +82,33 @@ function draw() {
 	} else {
 		alert("it doesn't look like your tool has a draw method!");
 	}
+	if (selectMode == 1) {
+		updatePixels();
+		noStroke();
+		fill(0,70,100,100);
+		rect(selectedArea.x, selectedArea.y, selectedArea.w, selectedArea.h);
+	}
 }
 
-function checkWithinCanvas(x,y) { //check that user mouse is on canvas, if it isn't, nothing will be drawn
+function checkWithinCanvas() { //check that user mouse is on canvas, if it isn't, nothing will be drawn
 	return (mouseX>=0 && mouseX<=canvasContainer.size().width && mouseY>=0 && mouseY<=canvasContainer.size().height)
 }
 
 function mousePressed() {
 	if (checkWithinCanvas()) saveState(); //allows for undoing/redoing, condition excludes clicking on tool icons
+	if (selectMode == 1) {
+		selectedArea.x = mouseX;
+		selectedArea.y = mouseY;
+	} else if (selectMode == 2 && checkWithinCanvas()) {
+		image(selectedPixels,mouseX - selectedPixels.width/2,mouseY - selectedPixels.height/2);
+	}
+}
+
+function mouseDragged() {
+	if (selectMode == 1) {
+		selectedArea.w = mouseX - selectedArea.x;
+		selectedArea.h = mouseY - selectedArea.y;
+	}
 }
 
 function keyPressed(e) {
